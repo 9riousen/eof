fe9c2c637c9ca16fdcdc7687963eb1fc118bd823
9riousen <9riousen@gmail.com>
2023-09-09 08:36:01 +0900

    테마 지원 제거

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,6 @@ import { themeChange } from 'theme-change';
 import { onMounted } from 'vue';
 import TxDialog from './components/TxDialog.vue';
 
-onMounted(() => {
-  themeChange(false);
-});
 </script>
 
 <template>
