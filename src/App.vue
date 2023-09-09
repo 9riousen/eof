58e8377b7650f528073b8026357ac192f86a3580
9riousen <9riousen@gmail.com>
2023-09-09 18:18:18 +0900

    테마 지우기

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <script setup lang="ts">
-import { themeChange } from 'theme-change';
-import { onMounted } from 'vue';
 import TxDialog from './components/TxDialog.vue';
-
-onMounted(() => {
-  themeChange(false);
-});
 </script>
 
 <template>
