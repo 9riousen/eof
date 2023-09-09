2d034bb8203f5fe06fce63064efaf8c38982760f
9riousen <9riousen@gmail.com>
2023-09-09 17:33:06 +0900

    Revert "테마 지원 제거"
    This reverts commit fe9c2c637c9ca16fdcdc7687963eb1fc118bd823.

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@ import { themeChange } from 'theme-change';
 import { onMounted } from 'vue';
 import TxDialog from './components/TxDialog.vue';
 
+onMounted(() => {
+  themeChange(false);
+});
 </script>
 
 <template>
